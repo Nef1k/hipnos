aec2481cb154ce1cb34e74b155c132be30ce26e6
Valeriy Nefedov <nef1k@outlook.com>
2023-05-11 07:34:26 +0300

    Add data models for Program subsystem. Massive doc update

--- be/hipnos/models.py
+++ be/hipnos/models.py
@@ -23,3 +23,40 @@ class Memory(models.Model):
     order_key = models.IntegerField(null=False)
 
     memory_type = models.ForeignKey(MemoryType, on_delete=models.DO_NOTHING)
+
+
+class HProgramState(models.Model):
+    LOCKED = 0
+    IN_PROGRESS = 1
+    FINISHED = 2
+
+    name = models.CharField(max_length=255, null=False, unique=True)
+
+    class Meta:
+        db_table = 'hipnos_program_state'
+
+
+class HipnosPhrase(models.Model):
+    name = models.CharField(max_length=255, null=False, unique=True)
+
+    phrase = models.CharField(max_length=255, null=False, unique=True)
+    unlocked_at = models.DateTimeField(null=True)
+
+    synonym = models.ForeignKey('HipnosPhrase', on_delete=models.CASCADE)
+    program = models.ForeignKey('HipnosProgram', null=True, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'hipnos_phrase'
+
+
+class HipnosProgram(models.Model):
+    name = models.CharField(max_length=255, null=False)
+    title = models.CharField(max_length=255, null=True, default='')
+
+    code_part = models.CharField(max_length=2, null=False)
+
+    state = models.ForeignKey(HProgramState, on_delete=models.DO_NOTHING)
+    target_phrase = models.ForeignKey(HipnosPhrase, on_delete=models.DO_NOTHING)
+
+    class Meta:
+        db_table = 'hipnos_program'
